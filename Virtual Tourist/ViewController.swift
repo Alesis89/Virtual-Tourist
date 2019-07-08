bd38ca405232c44bdf899e83268525b69316ffbf
Bill Clark <alesis89@gmail.com>
2019-07-07 19:05:35 -0500

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Bill Clark on 7/7/19.
+//  Copyright © 2019 Bill Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
